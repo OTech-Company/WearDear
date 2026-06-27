0bdc6de957dc1a31ac2dd7d1107c4a6e9557376b
Mohamed Ayman <2moo.ayman@gmail.com>
2026-06-27 13:05:02 +0300

    Initial Commit

---
+++ Carto/CartoApp.swift
@@ -0,0 +1,20 @@
+//
+//  CartoApp.swift
+//  Carto
+//
+//  Created by Mohamed Ayman on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CartoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
