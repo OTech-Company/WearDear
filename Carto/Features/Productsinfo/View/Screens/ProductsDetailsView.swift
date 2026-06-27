b0389534ddbb228f2f8602eecc15a2b2230cc4f0
Menna003 <mennaa101200@gmail.com>
2026-06-27 08:42:39 -0700

    add color-view ui

--- Carto/Features/Productsinfo/View/Screens/ProductsDetailsView.swift
+++ Carto/Features/Productsinfo/View/Screens/ProductsDetailsView.swift
@@ -11,14 +11,27 @@ struct ProductsDetailsView: View {
     var body: some View {
         VStack{
             HeaderView()
-            
             HStack{
+                ZStack{
+                    Image("nike")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Image("shoes")
+                        .resizable()
+                        .scaledToFit()
+                }
                 
+                ColorView()
+            }
+            VStack(alignment: .leading){
+                Text("$30.99")
+                    .bold()
+                Text("%10 OFF")
+                    .foregroundColor(.red)
             }
             
-            
-            
-        }
+        }.padding(8)
         
     }
 }
---
+++ Carto/Features/Productsinfo/View/UIComponents/ColorView.swift
@@ -0,0 +1,33 @@
+//
+//  ColorView.swift
+//  Carto
+//
+//  Created by Manona on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+
+    var body: some View {
+        VStack {
+            Text("Color")
+                .bold()
+            VStack{
+                Circle()
+                    .fill(.red)
+                    .frame(width: 25,height: 25)
+
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 25,height: 25)
+
+            }
+
+        }
+
+    }
+
+}
+
+
