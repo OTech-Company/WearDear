dcc7472c7e3d975a3fb2805a8d3dda7cb05e3328
Menna003 <mennaa101200@gmail.com>
2026-06-27 08:20:40 -0700

    implement header-view design

---
+++ Carto/Features/Productsinfo/View/Screens/ProductsDetailsView.swift
@@ -0,0 +1,26 @@
+//
+//  ProductsDetailsView.swift
+//  Carto
+//
+//  Created by Manona on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ProductsDetailsView: View {
+    var body: some View {
+        VStack{
+            HeaderView()
+            
+            HStack{
+                
+            }
+            
+            
+            
+        }
+        
+    }
+}
+
+
