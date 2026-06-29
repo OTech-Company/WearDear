0ff3758628a567f30e62dccd651e8a451fffafa6
MennaMohamed23 <116288453+MennaMohamed23@users.noreply.github.com>
2026-06-29 04:03:17 -0700

    implement product dto to domain mapper

---
+++ Carto/Core/Extensions/ProductDTO+Mapper.swift
@@ -0,0 +1,23 @@
+//
+//  ProductDTO+Mapper.swift
+//  Carto
+//
+//  Created by Manona on 28/06/2026.
+//
+
+
+extension ProductDTO {
+
+    func toDomain() -> ProductInfo {
+
+        ProductInfo(
+            id: id,
+            title: title,
+            price: Double(variants?.first?.price ?? "0") ?? 0,
+            discount: "",
+            imageURL: images?.first?.src ?? "",
+            sizes: options?.first(where: { $0.name.lowercased() == "size" })?.values ?? [],
+            colors: options?.first(where: { $0.name.lowercased() == "color" })?.values ?? []
+        )
+    }
+}
