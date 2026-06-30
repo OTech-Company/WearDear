47e019a3e1068e057296c841e4b509a8b48f5bc1
MO-Ayman22 <114579936+MO-Ayman22@users.noreply.github.com>
2026-06-30 18:53:28 +0300

    feat: add guest session persistence

--- Carto/Core/Extensions/String+Extensions.swift
+++ Carto/Core/Extensions/String+Extensions.swift
@@ -33,32 +33,4 @@ extension String {
                trimmed.range(of: regex, options: .regularExpression) != nil
     }
 }
-import Foundation
-
-extension String {
-
-    var isValidEmail: Bool {
-        let regex = /^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
-        return wholeMatch(of: regex) != nil
-    }
-
-    var isStrongPassword: Bool {
-        let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-        return wholeMatch(of: regex) != nil
-    }
-
-    var isWeakPassword: Bool {
-        !isStrongPassword
-    }
-    
-    var isValidName: Bool {
-        let trimmed = trimmingCharacters(in: .whitespacesAndNewlines)
-
-        let regex = #"^[A-Za-z]+(?:[ '-][A-Za-z]+)*$"#
-
-        return trimmed.count >= 2 &&
-               trimmed.count <= 50 &&
-               trimmed.range(of: regex, options: .regularExpression) != nil
-    }
-}
 
