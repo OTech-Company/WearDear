bdaaeed94d2a404214186bb38f9719376dfd0ac8
MennaMohamed23 <116288453+MennaMohamed23@users.noreply.github.com>
2026-06-30 02:45:20 -0700

    integrate favorites components into favorites view

--- Carto/Presentation/Favorites/View/Screens/FavoritesView.swift
+++ Carto/Presentation/Favorites/View/Screens/FavoritesView.swift
@@ -8,7 +8,41 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    let items = [
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        FavoriteItem(image: "shoes", title: "Nike", price: "$37,87", colors: [.cyan, .red]),
+        
+        
+    ]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
+        
+        VStack {
+            Text("Favorites")
+                .font(.title3)
+                .bold()
+                .padding(.top)
+            
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 18) {
+                    ForEach(items) { item in
+                        FavoriteCard(item: item)
+                    }
+                }
+                .padding()
+            }
+        }
+        .background(Color(.systemGray6))
         
     }
 }
